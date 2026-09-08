234c08f686747d52652bc885313ac926858b891a
saki_mori <185330635+sakimori@users.noreply.github.com>
2026-09-08 16:14:12 +0900

    fixed local_mode

--- app/main.py
+++ app/main.py
@@ -2,13 +2,12 @@ import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 import os
-import keep_alive
+import web_server
 
 load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
-NEED_KEEP_ALIVE = os.getenv("NEED_KEEP_ALIVE", "false").lower() == "true"
-
+LOCAL_MODE = os.getenv("LOCAL_MODE", "false").lower() == "true"
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -23,7 +22,7 @@ async def on_ready():
 async def nullpo(ctx):
     await ctx.send("ガッ")
 
-if NEED_KEEP_ALIVE:
-    keep_alive.keep_alive()
+if not LOCAL_MODE:
+    web_server.keep_alive()
 
 bot.run(TOKEN)
